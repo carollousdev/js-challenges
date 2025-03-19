1b9f714e76fa1737250acd78f087d5095f165bea
carollousdev <carollous.webdev@gmail.com>
2025-03-19 11:01:56 +0700

    Challenge Beginner I

---
+++ BEGINNER I/code.js
@@ -0,0 +1,52 @@
+// #QUESTION1 | HELLO WORLD
+function helloWorld() {
+    return 'Hello World!'
+}
+
+// #QUESTION2 | GET SUM
+const getSum = (a, b) => {
+    return a + b;
+}
+
+// #QUESTION3 | CALCULATOR
+const calculator = (a, b, operator) => {
+    switch (operator) {
+        case "+":
+            return a + b;
+            break;
+        case "-":
+            return a - b;
+            break;
+        case "*":
+            return a * b;
+            break;
+        case "/":
+            return a / b;
+            break;
+        default:
+            break;
+    }
+}
+
+// #QUESTION4 | COUNT OCCURRENCES
+const countOccurrences = (a, b) => {
+    const arr = a.split('');
+    let i = 0;
+    for (x = 0; x < a.length; x++) {
+        if (b === arr[x]) {
+            i++;
+        }
+    }
+    return i;
+}
+
+// #QUESTION5 | FIND MAX NUMBER
+const findMaxNumber = array => {
+    let i = 0;
+    for (x = 0; x <= array.length; x++) {
+        if (array[x] > i) {
+            i = x;
+        }
+    }
+    return i;
+}
